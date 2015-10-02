57fa5690432ad37898dd74d5e743cd489938d861
Karl R. Henderson <karlrhen@umich.edu>
2015-10-02 15:03:14 -0400

    First Push
    Initial Push

---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public static Player S;
+
+	void Awake(){
+		S = this;
+	}
+
+	void Start(){
+
+	}
+
+	void Update(){
+
+	}
+}
